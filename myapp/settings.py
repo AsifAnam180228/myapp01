a58eeb71125ed4a5383658e948d06556d2352659
Asif Anam <48431554+AsifAnam180228@users.noreply.github.com>
2020-07-04 12:01:32 +0600

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -46,9 +46,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
+    """'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ],
+    ],"""
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
 }
 
 MIDDLEWARE = [
